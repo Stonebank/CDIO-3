228ddd4e4e90c69699eb6045c8f145de33659f52
Tobias Mayn <tobiasmayn@gmail.com>
2023-03-01 11:16:57 +0100

    setup basic movement of robot

--- src/main.py
+++ src/main.py
@@ -6,15 +6,18 @@ import sys
 import time
 
 from ev3dev2.led import Leds
-from ev3dev2.motor import (OUTPUT_A, OUTPUT_B, LargeMotor, MoveTank,
-                           SpeedPercent)
+from ev3dev2.motor import (OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor,
+                           MediumMotor, MoveTank, SpeedPercent)
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 
 # state constants
 ON = True
-OFF = False
+OFF = False 
 
+# Initiate motors
+tank = MoveTank(OUTPUT_B, OUTPUT_C)
+rotate = MediumMotor(OUTPUT_A)
 
 def debug_print(*args, **kwargs):
     '''Print debug messages to stderr.
@@ -45,21 +48,23 @@ def set_font(name):
 
 def main():
     '''The main function of our program'''
-
+    for x in range(5):
+        print('Rotating...')
+        rotate.on_for_rotations(SpeedPercent(50), 5)
+        tank.on_for_rotations(50, 50, 5)
     # set the console just how we want it
-    reset_console()
-    set_cursor(OFF)
-    set_font('Lat15-Terminus24x12')
-
-    # print something to the screen of the device
-    print('Hello World!')
-
-    # print something to the output panel in VS Code
-    debug_print('Hello VS Code!')
+    #reset_console()
+    #set_cursor(OFF)
+    #set_font('Lat15-Terminus24x12')
+#
+    ## print something to the screen of the device
+    #print('Hello World!')
+#
+    ## print something to the output panel in VS Code
+    #debug_print('Hello VS Code!')
 
     # wait a bit so you have time to look at the display before the program
     # exits
-    time.sleep(5)
 
 if __name__ == '__main__':
     main()
