b783a9cf45a0c3ba671c10894fa85822c6ee1926
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2023-03-06 19:57:43 +0100

    Made mask for balls

--- src/modules/opencv_static.py
+++ src/modules/opencv_static.py
@@ -9,15 +9,27 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('image', image)
 
+
+
 lower_red = np.array([0, 0, 200], dtype = "uint8") 
 
 upper_red= np.array([100, 100, 255], dtype = "uint8")
 
-mask = cv2.inRange(image, lower_red, upper_red)
+lower_white = np.array([200, 200, 200], dtype = "uint8") 
 
-detected_output = cv2.bitwise_and(image, image, mask =  mask) 
+upper_white= np.array([255, 255, 255], dtype = "uint8")
 
-cv2.imshow("red color detection", detected_output) 
+whiteMask = cv2.inRange(image, lower_white, upper_white)
+
+redMask = cv2.inRange(image, lower_red, upper_red)
+
+detected_output_red = cv2.bitwise_and(image, image, mask = redMask) 
+
+detected_output_white = cv2.bitwise_and(image, image, mask = whiteMask) 
+
+cv2.imshow("red color detection", detected_output_red) 
+
+cv2.imshow("white color detection", detected_output_white) 
 
 cv2.waitKey(0) 
 
