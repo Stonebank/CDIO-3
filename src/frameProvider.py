cf591e60253af6c0c4457b3f4bacc8d8fcea41f2
Sebastian Gundestrup <sngp32@gmail.com>
2023-04-12 10:57:03 +0200

    Transform frame, find balls and find robot

---
+++ src/frameProvider.py
@@ -0,0 +1,110 @@
+import cv2
+import numpy as np
+
+
+class FrameTransformer:
+    manMode = False
+    selectCount = 0
+    # Field dimensions
+    width = 750
+    height = 500
+    corners = ([0,0],[750,0],[0,500],[750,500])
+    
+
+    def __init__(self):
+        pass
+
+    def transform(self, frame, frameCount):
+        if (not self.manMode and frameCount%20==0):
+            self.corners = self.getCorners(frame)
+
+        if (self.corners != None):
+            # Coordinates for the corners
+            oldCoordinates = np.float32([self.corners[0], self.corners[1],
+                                         self.corners[2], self.corners[3]])
+
+            # New coordinates for the corners
+            newCoordinates = np.float32([[0, 0], [self.width, 0],
+                                         [0, self.height], [self.width, self.height]])
+
+            # Transform image
+            matrix = cv2.getPerspectiveTransform(
+                oldCoordinates, newCoordinates)
+            transformed = cv2.warpPerspective(
+                frame, matrix, (self.width, self.height))
+            # Create circles to indicate detected corners
+            frame = cv2.circle(frame, tuple(self.corners[0]), 20, (255, 0, 0), 2)
+            frame = cv2.circle(frame, tuple(self.corners[1]), 20, (255, 0, 0), 2)
+            frame = cv2.circle(frame, tuple(self.corners[2]), 20, (255, 0, 0), 2)
+            frame = cv2.circle(frame, tuple(self.corners[3]), 20, (255, 0, 0), 2)
+            return transformed
+
+    def get_point(self, event, x, y, flags, param):
+
+        if event == cv2.EVENT_LBUTTONUP:
+            
+            self.selectCount += 1
+            if (self.selectCount == 1):
+                self.corners[0] = [x, y]
+                print("Upper left(", x, ",", y, ")")
+            elif (self.selectCount == 2):
+                self.corners[1] = [x, y]
+                print("Upper right(", x, ",", y, ")")
+            elif (self.selectCount == 3):
+                self.corners[2] = [x, y]
+                print("Lower left(", x, ",", y, ")")
+            elif (self.selectCount == 4):
+                self.corners[3] = [x, y]
+                print("Lower right(", x, ",", y, ")")
+                self.selectCount = 0
+
+    def getCorners(self, frame):
+        upper_left = [99999, 99999]
+        upper_right = [0, 99999]
+        lower_left = [99999, 0]
+        lower_right = [0, 0]
+
+        # Define lower/upper color field for red
+        lower_red = np.array([0, 0, 200], dtype="uint8")
+        upper_red = np.array([100, 100, 255], dtype="uint8")
+
+        # Frame dimensions
+        height = frame.shape[0]
+        width = frame.shape[1]
+
+        redMask = cv2.inRange(frame, lower_red, upper_red)
+
+        # Filter out everything that is not red
+        wall = frame.copy()
+        wall[np.where(redMask == 0)] = 0
+        # wall = cv2.GaussianBlur(wall,(41,41) ,5)
+        # Convert to grey
+        wall = cv2.cvtColor(wall, cv2.COLOR_BGR2GRAY)
+
+        # Find corners
+        # dest = cv2.cornerHarris(np.float32(wall), 20, 3, 0.2496)
+        dest = cv2.cornerHarris(np.float32(wall), 20, 3, 0.14)
+        dest = cv2.dilate(dest, None)
+
+        # frame[dest > 0.01 * dest.max()]=[0, 0, 255]
+
+        # Iterate through the detected corners, and use the appropiate ones
+        # TODO should be optimized in the future
+        thresh = 0.1*dest.max()
+        # if lower_right[0] == 0 :
+        for j in range(0, dest.shape[0]):
+            for i in range(0, dest.shape[1]):
+                if (dest[j, i] > thresh):
+                    if i < upper_left[0] and i < width/2 and j < upper_left[1] and j < height/2:
+                        upper_left[0] = i
+                        upper_left[1] = j
+                    if i > upper_right[0] and i > width/2 and j < upper_right[1] and j < height/2:
+                        upper_right[0] = i
+                        upper_right[1] = j
+                    if i < lower_left[0] and i < width/2 and j > lower_left[1] and j > height/2:
+                        lower_left[0] = i
+                        lower_left[1] = j
+                    if i > lower_right[0] and i > width/2 and j > lower_right[1] and j > height/2:
+                        lower_right[0] = i
+                        lower_right[1] = j
+        return [upper_left, upper_right, lower_left, lower_right]
--- src/main.py
+++ src/main.py
@@ -1,20 +1,92 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+import sys
+import threading
+import cv2
+import numpy as np
+
+from frameProvider import FrameTransformer
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+class Main:
+    def __init__(self):
+        # Set video input
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        ft = FrameTransformer()
+        frameCount = 0
+
+        while True:
+            frameCount += 1
+            ret, frame = cap.read()
+            transformed = ft.transform(frame, frameCount)
+            transformed = frame if transformed is None else transformed
+            self.findRobot(transformed)
+            self.findBalls(transformed)
+            if cv2.waitKey(1) == ord('m'):
+                if (not ft.manMode):
+                    ft.manMode = True
+                    cv2.namedWindow("Board")
+                    cv2.setMouseCallback("Board", ft.get_point)
+                    print("MANUAL MODE - input corners")
+                elif (ft.manMode):
+                    ft.manMode = False
+                    print("AUTO MODE")
+            elif cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            cv2.imshow("Transformed", transformed)
+            cv2.imshow("Board", frame)
+
+        cv2.destroyAllWindows()
+        sys.exit()
+
+    def findBalls(self, frame):
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        lower_white = np.array([200, 200, 200], dtype="uint8")
+        upper_white = np.array([255, 255, 255], dtype="uint8")
+
+        whiteMask = cv2.inRange(frame, lower_white, upper_white)
+
+        contours, hierarchy = cv2.findContours(
+            whiteMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+
+            (x, y, w, h) = cv2.boundingRect(contour)
+            radius = w/2
+            if (radius > 9 or radius < 6):
+                continue
+            cv2.putText(frame, "x: {}, y: {}, r: {}".format(int(x+w/2), int(y+h/2),
+                        int(w/2)), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(frame, (int(x + w / 2), int(y + h/2)),
+                       int(w/2), (0, 255, 0), 2)
+    
+    def findRobot(self, frame):
+        # range of rectangle green color 
+        lower_green = np.array([30, 42, 42])
+        upper_green = np.array([93, 255, 255])
+        # green color mask / convert frame to HSV color
+        greenMask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower_green, upper_green)
+        
+        # get the green rectangle 
+        greenRectangle = cv2.bitwise_and(frame, frame, mask=greenMask)
+        
+        # convert to gray
+        gray = cv2.cvtColor(greenRectangle, cv2.COLOR_BGR2GRAY)
+        
+        # catch the green rectangle
+        #threshold get a image out of a gray image
+        ret, threshGreen = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+        
+        # green rectangle contours 
+        #contours recognize the shape 
+        rectangleContours, hierarchy = cv2.findContours(threshGreen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        #  cheack for big Green area
+        if len(rectangleContours) > 0:
+            maxGreenArea = max(rectangleContours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(maxGreenArea)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
 
 
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here.
-ev3.speaker.beep()
+Main()
