ebd92617bcee0c96c63feffc43d8dd1e1ff47aa4
Sebastian Gundestrup <sngp32@gmail.com>
2023-04-21 16:26:57 +0200

    Added remote control capabilities for robot.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ import sys
 import threading
 import cv2
 import numpy as np
+import keyboard
 
 from frameProvider import FrameTransformer
+from remoteControl import Remote
 
 
 class Main:
@@ -11,34 +13,47 @@ class Main:
     robotX, robotY, robotWidth, robotHeight = 0, 0, 0, 0
 
     def __init__(self):
+        # Connect to robot
+        remote = Remote()
         # Set video input
-        cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-        ft = FrameTransformer()
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.ft = FrameTransformer()
         frameCount = 0
+
+        # Set hotkeys
+        keyboard.add_hotkey('m', lambda: (
+            self.toggleManMode()))
+        keyboard.add_hotkey('q', lambda: (
+            cap.release(),
+            cv2.destroyAllWindows(), sys.exit()))
 
         while True:
             frameCount += 1
             ret, frame = cap.read()
-            transformed = ft.transform(frame, frameCount)
+            transformed = self.ft.transform(frame, frameCount)
             transformed = frame if transformed is None else transformed
             self.findRobot(transformed)
             self.findBalls(transformed)
-            if cv2.waitKey(1) == ord('m'):
-                if (not ft.manMode):
-                    ft.manMode = True
-                    cv2.namedWindow("Board")
-                    cv2.setMouseCallback("Board", ft.get_point)
-                    print("MANUAL MODE - input corners")
-                elif (ft.manMode):
-                    ft.manMode = False
-                    print("AUTO MODE")
-            elif cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+
             cv2.imshow("Transformed", transformed)
             cv2.imshow("Board", frame)
-
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            # if (frameCount%20==0):
+            #     cv2.imwrite("robot.png", transformed)
+        cap.release()
         cv2.destroyAllWindows()
-        sys.exit()
+
+    # Toggle for manual corner selection
+    def toggleManMode(self):
+        if (not self.ft.manMode):
+            self.ft.manMode = True
+            cv2.namedWindow("Board")
+            cv2.setMouseCallback("Board", self.ft.get_point)
+            print("MANUAL MODE - input corners")
+        elif (self.ft.manMode):
+            self.ft.manMode = False
+            print("AUTO MODE")
 
     def findBalls(self, frame):
 
@@ -59,44 +74,47 @@ class Main:
             if (radius > 9 or radius < 6):
                 continue
             # filter if the robotX, robotY, robotWidth, robotHeight is in the ball area
-            if  x > self.robotX and x < self.robotX + self.robotWidth and y > self.robotY and y < self.robotY + self.robotHeight :
+            if x > self.robotX and x < self.robotX + self.robotWidth and y > self.robotY and y < self.robotY + self.robotHeight:
                 continue
             cv2.putText(frame, "x: {}, y: {}, r: {}".format(int(x+w/2), int(y+h/2),
                         int(w/2)), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             cv2.circle(frame, (int(x + w / 2), int(y + h/2)),
                        int(w/2), (0, 255, 0), 2)
-    
+
     def findRobot(self, frame):
-        # range of rectangle green color 
+        # range of rectangle green color
         lower_green = np.array([30, 42, 42])
         upper_green = np.array([93, 255, 255])
         # green color mask / convert frame to HSV color
-        greenMask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), lower_green, upper_green)
-        
-        # get the green rectangle 
+        greenMask = cv2.inRange(cv2.cvtColor(
+            frame, cv2.COLOR_BGR2HSV), lower_green, upper_green)
+
+        # get the green rectangle
         greenRectangle = cv2.bitwise_and(frame, frame, mask=greenMask)
-        
+
         # convert to gray
         gray = cv2.cvtColor(greenRectangle, cv2.COLOR_BGR2GRAY)
-        
+
         # catch the green rectangle
-        #threshold get a image out of a gray image
+        # threshold get a image out of a gray image
         ret, threshGreen = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-        
-        # green rectangle contours 
-        #contours recognize the shape 
-        rectangleContours, hierarchy = cv2.findContours(threshGreen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
+
+        # green rectangle contours
+        # contours recognize the shape
+        rectangleContours, hierarchy = cv2.findContours(
+            threshGreen, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         #  cheack for big Green area
         if len(rectangleContours) > 0:
             maxGreenArea = max(rectangleContours, key=cv2.contourArea)
-            robotX, robotY, robotWidth, robotHeight = cv2.boundingRect(maxGreenArea)
+            robotX, robotY, robotWidth, robotHeight = cv2.boundingRect(
+                maxGreenArea)
             self.robotX = robotX - 70
             self.robotY = robotY - 70
             self.robotWidth = robotWidth + 130
             self.robotHeight = robotHeight + 130
-            cv2.rectangle(frame, (self.robotX, self.robotY), (self.robotX+self.robotWidth, self.robotY+self.robotHeight), (0, 255, 0), 3)
-        
+            cv2.rectangle(frame, (self.robotX, self.robotY), (self.robotX +
+                          self.robotWidth, self.robotY+self.robotHeight), (0, 255, 0), 3)
 
 
 Main()
---
+++ src/remoteControl.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import rpyc
+
+
+
+class Remote:
+    def __init__(self):
+        # Create a RPyC connection to the remote ev3dev device.
+        # Use the hostname or IP address of the ev3dev device.
+        # If this fails, verify your IP connectivty via ``ping X.X.X.X``
+        print(rpyc.__version__)
+        print('attempting to connect')
+        conn = rpyc.classic.connect("169.254.165.31")
+        print('connected')
+
+        # import ev3dev2 on the remote ev3dev
+        self.ev3dev2_motor = conn.modules['ev3dev2.motor']
+        self.ev3dev2_sensor = conn.modules['ev3dev2.sensor.lego']
+
+        self.motor_right = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_C)
+        self.motor_left = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_D)
+        self.tank = self.ev3dev2_motor.MoveTank(self.ev3dev2_motor.OUTPUT_A, self.ev3dev2_motor.OUTPUT_B)
+
+        # dist_sensor = ev3dev2_sensor.ColorSensor()
+        self.tank.gyro = self.ev3dev2_sensor.GyroSensor()
+
+
+    def consume_balls(self):
+        self.motor_right.run_forever(speed_sp=600)
+        self.motor_left.run_forever(speed_sp=-600)
+
+
+    def eject_balls(self):
+        self.motor_right.run_forever(speed_sp=-600)
+        self.motor_left.run_forever(speed_sp=600)
+
+
+    def stop_balls_mec(self):
+        self.motor_left.stop()
+        self.motor_right.stop()
+
+
+    def go_forward(self):
+        self.tank.on(0, 50)
+
+    def go_forward_rotations(self):
+        self.tank.on_for_rotations(50, 50, 1)
+    
+    def go_back_rotations(self):
+        self.tank.on_for_rotations(-50, -50, 1)
+
+
+    def go_backwards(self):
+        self.tank.on(0, -50)
+
+
+    def go_left(self):
+        self.tank.on(-100, 10)
+
+
+    def go_right(self):
+        self.tank.on(100, 5)
+
+
+    def tank_turn_degrees(self, degrees):
+        self.tank.turn_degrees(
+            speed=self.ev3dev2_motor.SpeedPercent(15),
+            target_angle=degrees,
+            error_margin=3,
+        )
+
+
+    def stop_tank(self):
+        self.tank.stop()
